e357a632d1b6434b43e8ef6c80d62e47d6aa64ca
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-03 12:40:41 +0300

    feat: Add dropdown list to SortCategory.js

--- src/components/catalog/SortCategory.js
+++ src/components/catalog/SortCategory.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 
 const SortCategory = () => {
 
-    const [value, setValue] = useState('Популярные')
+    const [value, setValue] = useState('Выберите категорию')
 
     return (
         <>
             <fieldset className="sort__category">
                 <legend className="sort__title">Сортировка:</legend>
                 <input className="sort__category-value" value={value} disabled/>
+                <ul className="sort__category-list">
+                    <li className="sort__category-item" onClick={() => setValue('Популярные')}>Популярные</li>
+                    <li className="sort__category-item" onClick={() => setValue('Дешёвые')}>Дешёвые</li>
+                    <li className="sort__category-item" onClick={() => setValue('Дорогие')}>Дорогие</li>
+                </ul>
             </fieldset>
         </>
     );
