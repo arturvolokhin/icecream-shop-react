d64c95a21548670618b2caeadb3df614f7d80bef
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-03 12:13:23 +0300

    fix: Change the implementation of the hover effect: 1) Remove a state from a component ProductsItem.js 2) Make a hover effect using css.

--- src/components/catalog/ProductsItem.js
+++ src/components/catalog/ProductsItem.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 
 const ProductsItem = ({ price, image, description }) => {
-    const [hover, setHover] = useState(false);
-
     return (
-        <div
-            className="products__item"
-            onMouseEnter={() => setHover(!hover)}
-            onMouseLeave={() => setHover(!hover)}
-        >
+        <div className="products__item">
             <img className="products__image" src={image} alt="products item" />
             <p className="products__price">
                 {price} &#8381;<span>/кг</span>
             </p>
             <p className="products__description">{description}</p>
-            {hover && (
-                <p className="products__item-hint">Кликните, чтобы добавить в корзину.</p>
-            )}
+            <p className="products__item-hint">
+                Кликните, чтобы добавить в корзину.
+            </p>
         </div>
     );
 };
