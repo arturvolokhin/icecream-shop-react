f426b2c6b5d149fbaf0342682692c5b050d816cb
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-03 13:31:29 +0300

    feat: Add custom radio-button to SortFat.js

--- src/components/catalog/SortFat.js
+++ src/components/catalog/SortFat.js
@@ -3,20 +3,24 @@ const SortFat = () => {
         <>
             <fieldset className="sort__fat">
                 <legend className="sort__title">Жирность:</legend>
-                <label>
+                <label className="sort__fat-label">
                     <input className="sort__fat-radio" type="radio" name="fat" value="0" />
+                    <div className="sort__fat-radio-custom"/>
                     0%
                 </label>
-                <label>
+                <label className="sort__fat-label">
                     <input className="sort__fat-radio" type="radio" name="fat" value="10" />
+                    <div className="sort__fat-radio-custom"/>
                     До 10%
                 </label>
-                <label>
+                <label className="sort__fat-label">
                     <input className="sort__fat-radio" type="radio" name="fat" value="30" />
+                    <div className="sort__fat-radio-custom"/>
                     До 30%
                 </label>
-                <label>
+                <label className="sort__fat-label">
                     <input className="sort__fat-radio" type="radio" name="fat" value="100" />
+                    <div className="sort__fat-radio-custom"/>
                     Выше 30%
                 </label>
             </fieldset>
