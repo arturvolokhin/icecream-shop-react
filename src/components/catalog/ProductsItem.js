f79d8b5f26d2f4cd99e7c6c1ccb1c2bff09c4758
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-03 12:06:30 +0300

    feat: Add hover-effect to ProductsItem

--- src/components/catalog/ProductsItem.js
+++ src/components/catalog/ProductsItem.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
+
 const ProductsItem = ({ price, image, description }) => {
+    const [hover, setHover] = useState(false);
+
     return (
-        <div className="products__item">
+        <div
+            className="products__item"
+            onMouseEnter={() => setHover(!hover)}
+            onMouseLeave={() => setHover(!hover)}
+        >
             <img className="products__image" src={image} alt="products item" />
             <p className="products__price">
-                {price} + &#8381;<span>/кг</span>
+                {price} &#8381;<span>/кг</span>
             </p>
             <p className="products__description">{description}</p>
+            {hover && (
+                <p className="products__item-hint">Кликните, чтобы добавить в корзину.</p>
+            )}
         </div>
     );
 };
