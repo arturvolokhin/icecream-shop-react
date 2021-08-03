3c4d2ba3533a75143d35a558b3ac8b32409129c7
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-03 15:00:33 +0300

    feat: Add input range in SortCost.js

--- src/components/catalog/SortCost.js
+++ src/components/catalog/SortCost.js
@@ -1,12 +1,29 @@
-import Slider, { Range } from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import React, { useState } from "react";
+import "rc-slider/assets/index.css";
+import Slider from "rc-slider";
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
 
 const SortCost = () => {
+    const [value, setValue] = useState([0, 450]);
+
+    const handleChange = (value) => {
+        setValue(value);
+    };
+
     return (
         <>
             <fieldset className="sort__cost">
-                <legend className="sort__title">Цена:</legend>
-                <Range />
+                <legend className="sort__title">
+                    Цена: {value[0]} руб - {value[1]} руб
+                </legend>
+                <Range
+                    defaultValue={[0, 450]}
+                    allowCross={false}
+                    min={0}
+                    max={450}
+                    onChange={handleChange}
+                />
             </fieldset>
         </>
     );
