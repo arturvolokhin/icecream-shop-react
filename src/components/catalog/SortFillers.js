403405bd581f030bcd35f44a0584d52580d674d3
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-03 13:47:31 +0300

    feat: Add custom checkbox to SortFillers.js

--- src/components/catalog/SortFillers.js
+++ src/components/catalog/SortFillers.js
@@ -3,49 +3,54 @@ const SortFillers = () => {
         <fieldset className="sort__fillers">
             <legend className="sort__title">Наполнители:</legend>
 
-            <label>
+            <label className="sort__fillers-label">
                 <input
                     className="sort__fillers-checkbox"
                     type="checkbox"
                     name="fillers"
                     value="chocolate"
                 />
+                <div className="sort__fillers-checkbox-custom"/>
                 Шоколадные
             </label>
-            <label>
+            <label className="sort__fillers-label">
                 <input
                     className="sort__fillers-checkbox"
                     type="checkbox"
                     name="fillers"
                     value="sugar"
                 />
+                <div className="sort__fillers-checkbox-custom"/>
                 Сахарные присыпки
             </label>
-            <label>
+            <label className="sort__fillers-label">
                 <input
                     className="sort__fillers-checkbox"
                     type="checkbox"
                     name="fillers"
                     value="fruit"
                 />
+                <div className="sort__fillers-checkbox-custom"/>
                 Фрукты
             </label>
-            <label>
+            <label className="sort__fillers-label">
                 <input
                     className="sort__fillers-checkbox"
                     type="checkbox"
                     name="fillers"
                     value="syrup"
                 />
+                <div className="sort__fillers-checkbox-custom"/>
                 Сиропы
             </label>
-            <label>
+            <label className="sort__fillers-label">
                 <input
                     className="sort__fillers-checkbox"
                     type="checkbox"
                     name="course_name"
                     value="jam"
                 />
+                <div className="sort__fillers-checkbox-custom"/>
                 Джемы
             </label>
         </fieldset>
